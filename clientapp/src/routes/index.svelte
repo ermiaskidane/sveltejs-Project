3f8d214cd50e3b26c7123ce8a9818de2b8d2261b
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2021-07-15 19:55:06 +0100

    Update index.svelte

--- clientapp/src/routes/index.svelte
+++ clientapp/src/routes/index.svelte
@@ -17,7 +17,7 @@
 <h1 class="text-4xl text-center my-8 uppercase">SvelteKit Project</h1>
 
 <div class="container py-2 mx-auto">
-	<div class="p-2">
+	<div class="p-3">
 		<ul class="grid sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-screen-lg mt-16">
 			<li class="border py-4 px-2 flex flex-col items-center lg:py-6 shadow-xl ">
 				<h2 class="pb-4 text-2xl">Ermias Kidane</h2>
