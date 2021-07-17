910173161dfd244bc8ae86d624fa1a1ba8444a51
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2021-07-17 23:05:04 +0100

    Update studentRoutes.js

--- serverapp/routes/studentRoutes.js
+++ serverapp/routes/studentRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const studentController = require("../controllers/studentController")
 
 router.get("/", studentController.getAllStudent)
+router.get("/:studId", studentController.getOneStudent)
 router.post("/", studentController.addStudent)
 router.put("/:studId", studentController.updateStudent)
 router.delete("/:studId", studentController.deleteStudent)
