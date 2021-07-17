f486461f52ef395b08f62eb3f64160a9a94a33a5
ermias kidane <hadeklte21@gmail.com>
2021-07-17 22:57:02 +0100

    added the add student and fetch a student

---
+++ clientapp/src/routes/students/[id].svelte
@@ -0,0 +1,26 @@
+<script context="module">
+    export async function load({ page }) {
+        const id = page.params.id
+        const url = `http://localhost:5003/api/student/${id}`
+        const res = await fetch(url);
+        const student = await res.json();
+        console.log(student)
+        return {props: {student}}
+    }
+
+</script>
+
+<script>
+    export let student;
+</script>
+
+<div class=" border py-4 px-2  lg:py-6 shadow-xl">
+    <h2 class="pb-4 text-2xl capitalize">{student.name}</h2>
+    <p class=""><span class="capitalize">grade</span>: {student.grade}</p>
+    <p class=""><span class="capitalize">gender</span>: {student.gender}</p>
+    <p class=""><span class="capitalize">School</span>: {student.school}</p>
+    <div class="flex">
+        <button>Edit</button>
+        <button>Delete</button>
+    </div>
+</div>
