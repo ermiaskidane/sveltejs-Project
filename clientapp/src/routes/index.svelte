c40fbab429456699448120c0733b38b0d35e1300
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2021-08-05 13:32:08 +0100

    Update index.svelte

--- clientapp/src/routes/index.svelte
+++ clientapp/src/routes/index.svelte
@@ -1,8 +1,21 @@
+<script context="module">
+	export async function load({page}) {
+		const baseUrl = 'http://localhost:5003/api/student';
+		const res = await fetch(baseUrl);
+		const data = await res.json();
+		console.log(data)
+		return {
+			props: {students: data}
+		}
+		
+	}
+</script>
 <script>
 	import { onMount } from 'svelte';
 	import { fade } from 'svelte/transition';
 	const baseUrl = 'http://localhost:5003/api/student';
 // 	let students = [];
+	export let students
 
 // 	onMount(async () => {
 // 		let res = await fetch(baseUrl);
