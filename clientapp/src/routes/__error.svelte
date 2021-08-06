0cd98f22bc293174b3c4d1e34ea1fba530120e91
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2021-08-06 12:58:20 +0100

    Add files via upload

---
+++ clientapp/src/routes/__error.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+	export function load({ error, status }) {
+		return {
+			props: {
+				error,
+				status
+			}
+		};
+	}
+</script>
+
+<script>
+	export let status;
+	export let error;
+	console.log(error);
+</script>
+
+<div class="flex flex-col gap-4 items-center bg-gradient-to-b from-gray-300 py-8 font-mono">
+	<h1 class="text-4xl ">SOMETHING HAPPEND!</h1>
+
+	<h3 class="text-2xl">{status}</h3>
+	<h3 class="text-2xl">{error.message}</h3>
+</div>
